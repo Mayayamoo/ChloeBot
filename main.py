03b42f7396945860bdb7aeb44a74a4b6bbaba7a2
Ashe Muller <83967304+MaleVTuber@users.noreply.github.com>
2022-02-05 08:43:03 -0500

    Ashe has made a pull request :O
    Removed useless space on one string in list, fixed command prefix, added a few commands for no reason

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ import random
 from keep_alive import keep_alive
 from discord.ext import commands
 
+client = commands.Bot(command_prefix="!") # Use Discord.py 2.0 if you want to use slash commands. This stuff doesn't work :p
 
-client = commands.Bot(command_prefix="/")
-
-
-magic8= [' it is likely', 'no shot bucko', 'lol', 'probably', '...', 'im a bot, how am i supposed to know', 'sure, whatever', 'why do *you* want to know?', 'TW/// NO', 'OFC! not', 'yeah sure', 'probably', 'i give it a 90% chance', 'my creator didnt code a response for this dumb fucking question', 'what a waste of a question', 'if you want', 'ok', 'ask Chloe', 'i just want to wear thigh highs' ]
+magic8= ['it is likely', 'no shot bucko', 'lol', 'probably', '...', 'im a bot, how am i supposed to know', 'sure, whatever', 'why do *you* want to know?', 'TW/// NO', 'OFC! not', 'yeah sure', 'probably', 'i give it a 90% chance', 'my creator didnt code a response for this dumb fucking question', 'what a waste of a question', 'if you want', 'ok', 'ask Chloe', 'i just want to wear thigh highs' ]
 
 @client.event
 async def on_ready():
@@ -22,13 +20,15 @@ async def maya(ctx):
 async def m8b(ctx):
   lucky_num = random.randint(0,len(magic8) - 1)
   await ctx.send(magic8[lucky_num])
-
-
+    
 @client.command()
 async def gwenvara(ctx):
   await ctx.send('the most based mod!')
+
 @client.command()
 async def true(ctx):
   await ctx.send('so true!')
+
 keep_alive()
+
 client.run(os.getenv('TOKEN'))
